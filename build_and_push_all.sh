8bb1420f054fc4b64e86f343eacf1d8faf307fc0
GreenAddress <info@greenaddress.it>
2015-11-18 18:02:44 +0100

    Added web-only js files to be able to generate complete djangojs, regenerated djangojs

--- build_and_push_all.sh
+++ build_and_push_all.sh
@@ -13,12 +13,12 @@ git clone git@github.com:greenaddress/WalletCrx.git
 cp WalletCrx/static/wallet/config{,_regtest,_testnet}.js /tmp
 python render_templates.py WalletCrx
 rm -rf WalletCrx/static
-rm -rf WalletCrx/fi
 cp -r static WalletCrx/static
 rm -rf WalletCrx/static/fonts/*.svg  # .woff are enough for crx
 rm -rf WalletCrx/static/sound/*.wav  # .mp3 are enough for crx
 rm WalletCrx/static/js/cdv-plugin-fb-connect.js  # cordova only
 rm WalletCrx/static/js/facebook-js-sdk.js  # cordova only
+rm WalletCrx/static/js/{greenaddress,instant}.js  # web only
 mkdir -p WalletCrx/static/wallet >/dev/null
 mv /tmp/config{,_regtest,_testnet}.js WalletCrx/static/wallet/
 
@@ -38,10 +38,10 @@ git clone git@github.com:greenaddress/WalletCordova.git
 cp WalletCordova/www/greenaddress.it/static/wallet/config.js /tmp/config.js
 python render_templates.py -a WalletCordova/www/greenaddress.it
 rm -rf WalletCordova/www/greenaddress.it/static
-rm -rf WalletCordova/fi
 cp -r static WalletCordova/www/greenaddress.it/static
 rm -rf WalletCordova/www/greenaddress.it/static/js/jsqrcode  # crx only
 rm -rf WalletCordova/www/greenaddress.it/static/js/btchip-js-api  # crx only
+rm WalletCordova/static/js/{greenaddress,instant}.js  # web only
 mkdir -p WalletCordova/www/greenaddress.it/static/wallet >/dev/null
 mv /tmp/config.js WalletCordova/www/greenaddress.it/static/wallet/config.js
 
