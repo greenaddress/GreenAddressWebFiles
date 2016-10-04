11d947f1ebcf73272d1039f400efcb56f67dfcef
Jerzy Kozera <jerzy.kozera@gmail.com>
2016-10-04 16:36:43 +0200

    Add promise.prototype.finally shim

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,8 @@
 var Window = require('global/window');
+
+var promiseFinally = require('promise.prototype.finally');
+promiseFinally.shim();
+
 // load libs, this is basically a shame folder
 require('./lib');
 
