8a42af345cb10e662d1cbaa9db62834e2de417f5
Jerzy Kozera <jerzy.kozera@gmail.com>
2017-05-02 09:02:32 +0200

    Initial support for new assets in Electron
     - tx deserialization
     - unblinding

--- static/js/greenwallet/wallet/ga-impl/index.js
+++ static/js/greenwallet/wallet/ga-impl/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   GAWallet: require('./ga-wallet'),
   AssetsWallet: require('./assets-wallet'),
+  ConfidentialUtxo: require('./confidential-utxo'),
   Service: require('./service'),
   UtxoFactory: require('./utxo-factory').GAUtxoFactory,
   Utxo: require('./utxo-factory').GAUtxo,
