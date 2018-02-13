49609eb267dac62a79abb3770d548ac42385e4be
Lawrence Nahum <lawrence@greenaddress.it>
2018-02-13 18:29:38 +0100

    Update libwally reference to latest fixed commit

--- fetch_libwally.sh
+++ fetch_libwally.sh
@@ -5,5 +5,5 @@ set -e
 # Add the wally plugin:
 if [ \! -e libwally-core ]; then
     git clone https://github.com/ElementsProject/libwally-core.git libwally-core
-    (cd libwally-core && git checkout f76881286d979b0592996fa3c57dc02e6faec443)
+    (cd libwally-core && git checkout c628a5af1d9ede89d142d70abaf4c4f09f0e3bc9)
 fi
