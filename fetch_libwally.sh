ad5467a902a32e0c2bd6fd8df9eb50868d7d457e
Lawrence Nahum <lawrence@greenaddress.it>
2018-06-12 16:26:53 +0200

    update wally to commit 688e10454a4f5bd77fd847170c22ae5e8a84a7bb

--- fetch_libwally.sh
+++ fetch_libwally.sh
@@ -5,5 +5,5 @@ set -e
 # Add the wally plugin:
 if [ \! -e libwally-core ]; then
     git clone https://github.com/ElementsProject/libwally-core.git libwally-core
-    (cd libwally-core && git checkout c628a5af1d9ede89d142d70abaf4c4f09f0e3bc9)
+    (cd libwally-core && git checkout 688e10454a4f5bd77fd847170c22ae5e8a84a7bb)
 fi
