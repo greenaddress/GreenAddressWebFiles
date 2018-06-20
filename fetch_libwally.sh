4d890bcb9db0168d927fcc2da95c1f3f691abbe6
Lawrence Nahum <lawrence@greenaddress.it>
2018-06-20 13:59:17 +0200

    update to latest libwally w/ wally_format_bitcoin_message js bindings

--- fetch_libwally.sh
+++ fetch_libwally.sh
@@ -5,5 +5,5 @@ set -e
 # Add the wally plugin:
 if [ \! -e libwally-core ]; then
     git clone https://github.com/ElementsProject/libwally-core.git libwally-core
-    (cd libwally-core && git checkout 688e10454a4f5bd77fd847170c22ae5e8a84a7bb)
+    (cd libwally-core && git checkout de8ae9c474f362577e315a15967ea8172ca819aa)
 fi
