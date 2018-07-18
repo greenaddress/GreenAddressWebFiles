e22bbeea35ee642111adaa85445ee5a68a192f23
Lawrence Nahum <lawrence@greenaddress.it>
2018-07-18 23:01:47 +0200

    update JS libs

--- fetch_libwally.sh
+++ fetch_libwally.sh
@@ -4,6 +4,6 @@ set -e
 
 # Add the wally plugin:
 if [ \! -e libwally-core ]; then
-    git clone https://github.com/ElementsProject/libwally-core.git libwally-core
-    (cd libwally-core && git checkout d0a13e08b51fccf420e2be255e3c959831637aef)
+    git clone https://github.com/greenaddress/libwally-core.git libwally-core
+    (cd libwally-core && git checkout e942864f73d76f372dfeee6d1c7aa048c5b8b5c2)
 fi
