26fdb17fd5328e97745890b0e886c3d1f092c903
Lawrence Nahum <lawrence@greenaddress.it>
2019-09-27 16:18:31 +0200

    update to debian buster, update node to 10

--- contrib/buster_deps.sh
+++ contrib/buster_deps.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-sed -i 's/deb.debian.org/httpredir.debian.org/g' /etc/apt/sources.list
 apt-get -yqq update && apt-get -yqq upgrade
 apt-get -yqq install unzip git curl build-essential python-virtualenv python-pip make swig autoconf libtool pkg-config lib32z1 python-dev libffi-dev virtualenv
-curl -sL https://deb.nodesource.com/setup_8.x | bash -
+curl -sL https://deb.nodesource.com/setup_10.x | bash -
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get -yqq update
